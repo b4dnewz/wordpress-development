ece26d3cf3ae936abbebdfbe1ab1323e81daaa50
Filippo Conti <filippo@codekraft.it>
2017-09-30 10:19:31 +0200

    Added reference link to footer

--- theme-name/theme-files/footer.php
+++ theme-name/theme-files/footer.php
@@ -8,7 +8,7 @@
 
 					<p>
 						<?php printf( __('Made with &hearts; by %s.', 'wordpress-theme-starter'), '<a href="https://codekraft.it" title="codekraft-studio">codekraft-studio</a>' ); ?>
-						<?php _e('Powered by', 'wordpress-theme-starter'); ?> <a href="//wordpress.org" title="WordPress">WordPress</a>.
+						<?php _e('Powered by', 'wordpress-theme-starter'); ?> <a href="//wordpress.org" title="WordPress">WordPress</a> and <a href="//webpack.js.org/" title="Webpack">Webpack</a>.
 					</p>
 
 				</div>
