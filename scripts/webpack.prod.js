9ba6706507a1dac3d490703405cdf25a4738681d
Filippo Conti <filippo@codekraft.it>
2019-02-17 10:23:19 +0100

    Updated development dependencies
    Updated webpack to latest version, added new webpack-cli dependency
    Removed extract-text-webpack-plugin which dropped support for webpack 4 in favor of mini-css-extract-plugin
    Updated the base webpack configuration
    Moved babel configuration in dedicated file (recommended)

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,20 +1,24 @@
 const path = require('path');
 const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const compiler = require('./webpack.base').compiler;
+
+compiler.mode = 'production';
 
 // Set the production source map type
 compiler.devtool = 'source-map';
 
 // Uglify scripts for production use
-compiler.plugins.push(new webpack.optimize.UglifyJsPlugin({
-  minimize: true,
-  sourceMap: false,
-  output: {
-    comments: false
-  }
-}));
+compiler.optimization = {
+  minimizer: [new UglifyJsPlugin({
+    cache: true,
+    parallel: true,
+    sourceMap: true
+  })]
+};
 
+// Copy whole theme, assets and built files into dist
 compiler.plugins.push(new CopyWebpackPlugin([
   {
     from: path.resolve(__dirname, '../src/theme'),
