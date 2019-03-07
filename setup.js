100a6bbb6c2a9937c4ca9d9abb7def6daa460a2e
Filippo Conti <filippo@codekraft.it>
2019-03-07 08:40:21 +0100

    Updated setup script

--- setup.js
+++ setup.js
@@ -2,8 +2,10 @@
 
 const fs = require('fs')
 const path = require('path')
+const del = require('del');
 const readline = require('readline')
 const pkg = require('./package.json')
+const execSync = require('child_process').execSync
 const dirName = path.basename(__dirname)
 if (dirName === pkg.name) {
   return
@@ -15,32 +17,56 @@ if (fs.existsSync(envFile)) {
   return
 }
 
-const rl = readline.createInterface({input: process.stdin, output: process.stdout})
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-rl.question(`What is your project name? (${dirName}) `, (answer) => {
-  rl.close()
-  answer = (answer && answer !== '')
-    ? answer
+// Collect answers
+const answers = {}
+
+rl.question(`What is your project name? (${dirName}) `, (project) => {
+  answers.projectName = (project && project !== '')
+    ? project
     : dirName
 
-  console.log('Creating the .env file from template')
-  fs.copyFileSync(`${envFile}.example`, envFile)
+  rl.question(`Which WordPress version do you want? (latest) `, (version) => {
+    answers.wordpressVersion = (version && version !== '')
+      ? version
+      : 'latest'
 
-  console.log('Updating .env variables using:', answer)
-  const envVariables = fs.readFileSync(envFile, 'utf8')
-  fs.writeFileSync(envFile, envVariables.replace('wordpress.development', answer), 'utf8')
+    rl.close()
 
-  console.log('Updating package.json variables')
-  const newPkg = Object.assign({}, pkg, {
-    name: answer,
-    description: "",
-    version: "0.1.0",
-    author: ""
+    const newPkg = Object.assign({}, pkg, {
+      name: answers.projectName,
+      description: "",
+      version: "0.1.0",
+      author: ""
+    })
+
+    const envVariables = fs.readFileSync(`${envFile}.example`, 'utf8')
+      .replace('HOSTNAME=wordpress.development', `HOSTNAME=${answers.projectName}`)
+      .replace('WORDPRESS_VERSION=latest', `WORDPRESS_VERSION=${answers.wordpressVersion}`)
+
+    console.log('\nCreating the .env file')
+    fs.writeFileSync(envFile, envVariables, 'utf8')
+    fs.unlinkSync(`${envFile}.example`)
+
+    console.log('Updating package.json variables')
+    fs.writeFileSync(
+      path.resolve(__dirname, 'package.json'),
+      JSON.stringify(newPkg, null, 2),
+      'utf-8'
+    )
+
+    try {
+      del.sync('./.git')
+      execSync('git init')
+    } catch (e) {
+      console.log('Failed to recreate git repostiory:');
+      console.log(e.message);
+    }
+
+    console.log('Finished project initialization\n');
   })
-  fs.writeFileSync(
-    path.resolve(__dirname, 'package.json'),
-    JSON.stringify(newPkg, null, 2),
-    'utf-8'
-  )
-  console.log('Finished project initialization\n');
 })
